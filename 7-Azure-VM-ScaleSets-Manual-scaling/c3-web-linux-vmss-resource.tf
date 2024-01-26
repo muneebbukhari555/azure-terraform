6fa2c463854eeb7d5aabae49b5a364d035f20e76
Muneeb Ahmad <mahmad.itp@sparkcognition.com>
2024-01-26 19:42:58 +0500

    adding tf files for vmss and lb

---
+++ 7-Azure-VM-ScaleSets-Manual-scaling/c3-web-linux-vmss-resource.tf
@@ -0,0 +1,41 @@
+resource "azurerm_linux_virtual_machine_scale_set" "web_vmss" {
+  name                = "${local.resource_name_prefix}-web-vmss"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard_B1s"
+  instances           = 3
+  admin_username      = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
+    
+
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "83-gen2"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  upgrade_mode = "Automatic"
+
+  network_interface {
+    name                      = "web-vmss-nic"
+    primary                   = true
+    network_security_group_id = azurerm_network_security_group.web_vmss_nsg.id
+    ip_configuration {
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.websubnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id]
+    }
+  }
+  custom_data = filebase64("${path.module}/app-scripts/cloud-init-apache.yaml")
+}
